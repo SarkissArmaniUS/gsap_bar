bf76c76e7dec3f4e897768109fa8ba16bc0279e0
пк <pk@MacBook-Pro-pk.local>
2026-04-18 08:19:53 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import gsap from 'gsap';
+import { ScrollTrigger, SplitText } from "gsap/all";
+
+const App = () => {
+  return (
+    <div>
+      <h1>App</h1>
+    </div>
+  )
+}
+
+export default App
